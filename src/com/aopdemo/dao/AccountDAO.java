fa54449967335879e4e47bb521f435e8cbe46747
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-26 22:39:19 +0000

    Cleaning Up - Project copied

---
+++ src/com/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,16 @@
+package com.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+
+	
+	public void addAccount() {
+		
+		System.out.println(getClass()+": DOING MY DB WORK");
+		
+	}
+	
+	
+}
