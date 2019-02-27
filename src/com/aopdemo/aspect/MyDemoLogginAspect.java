cc303677cae80693d6eb6916441b4c58539278dc
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-27 23:00:47 +0000

    Pointcut Expressions 2
    Demo

--- src/com/aopdemo/aspect/MyDemoLogginAspect.java
+++ src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -11,7 +11,7 @@ public class MyDemoLogginAspect {
 	//RELATED ADVICES FOR LOGGING
 	
 	//@BEFORE
-	@Before("execution(public void addAccount())")
+	@Before("execution(public void com.aopdemo.dao.AccountDAO.addAccount())")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("Executing @Before advice on addAccount()");
